65fddaccefb3fd16668ba74baa85e17c1c3cc87b
Pierrick Bruneau <pierrick.bruneau@list.lu>
2022-11-22 16:40:22 +0100

    added numba support to softsupervised benchmark

--- quickstart.py
+++ quickstart.py
@@ -31,16 +31,16 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dace", type=util.str2bool, nargs="?", default=True)
     args = vars(parser.parse_args())
 
-    #benchmarks = [
-    #    'adi', 'arc_distance', 'atax', 'azimint_naive', 'bicg', 'cavity_flow',
-    #    'cholesky2', 'compute', 'convolutional', 'doitgen', 'floyd_warshall', 'gemm', 'gemver',
-    #    'gesummv', 'go_fast', 'hdiff', 'jacobi_2d', 'mlp', 'softsupervised',
-    #    'syr2k', 'trmm', 'vadv'
-    #]
-    benchmarks = [ 'convolutional' ]
+    benchmarks = [
+        'adi', 'arc_distance', 'atax', 'azimint_naive', 'bicg', 'cavity_flow',
+        'cholesky2', 'compute', 'doitgen', 'floyd_warshall', 'gemm', 'gemver',
+        'gesummv', 'go_fast', 'hdiff', 'jacobi_2d', 'mlp', 'softsupervised',
+        'syr2k', 'trmm', 'vadv'
+    ]
     
-    #frameworks = ["numpy", "numba", "cupy", "cupyswapper"]
-    frameworks = ["numpy", "cupy", "cupyswapper"]
+    #benchmarks = [ 'convolutional' ]
+    
+    frameworks = ["numpy", "numba", "cupy", "cupyswapper"]
     #if args['dace']:
     #    frameworks.append("dace_cpu")
 
